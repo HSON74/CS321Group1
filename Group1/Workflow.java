8df69f73edf134f19b15338bd51ca42dd74d4bac
HSON74 <1442386@gmail.com>
2024-02-27 10:12:22 -0500

    Create Workflow.java

---
+++ Group1/Workflow.java
@@ -0,0 +1,22 @@
+
+public class Workflow {
+    private Integer wfid;
+    private String step;
+    private Integer objid;
+
+    public Boolean AddWFltem(String step, Integer onjid) {
+        return new Boolenan();
+    }
+
+    public Integer GetNextWFltem(String step) {
+        return -1;
+    }
+
+    public Integer CountWFltems(String step) {
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
