046e42d864cd9907291c61de541ae7d57a759215
HSON74 <115472952+HSON74@users.noreply.github.com>
2024-03-21 11:09:54 -0400

    update 4

--- sample/src/main/java/org/openjfx/Business/Window.java
+++ sample/src/main/java/org/openjfx/Business/Window.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
-public class main extends Application {
+public class Window extends Application {
     public static void main(String args[]) {
         launch(args);
     }
