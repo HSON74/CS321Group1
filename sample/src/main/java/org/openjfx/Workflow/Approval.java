4414c70668128697d39cb7237e76402e4b42d223
HSON74 <115472952+HSON74@users.noreply.github.com>
2024-03-30 12:01:14 -0400

    Update 14

--- sample/src/main/java/org/openjfx/Workflow/Approval.java
+++ sample/src/main/java/org/openjfx/Workflow/Approval.java
@@ -1,12 +1,17 @@
 package org.openjfx.Workflow;
 
 import org.openjfx.Business.Form;
+import org.openjfx.Business.Immigrant;
 
 import javafx.scene.*;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.*;
 
@@ -26,20 +31,38 @@ public class Approval {
     public void Adisplay(Form form, Workflow system, Stage primaryStage) {
         this.approvalForm = form;
         this.approvalWorkflow = system;
-        int x_grid = 10;
-        int y_grid = 50;
+        Text[] approvalTextsI = { new Text("First Name: "), new Text("Middle Name:"), new Text("Last Name:"),
+                new Text("Age: "), new Text("Birth Month:"), new Text("Birth Day:"), new Text("Birth Year: "),
+                new Text("Address: "), new Text("Social Secrutiy Numbers:"), new Text("Race: "), new Text("Gender: "),
+                new Text("Married Status: "), new Text("Phone Numbers: "), new Text("Mother Name: "),
+                new Text("Father Name: "), new Text("Employment Status: "), new Text("Immigrant PID: "),
+                new Text("Immirgrant's Dependent PID: "), };
+        Text[] approvalTextsD = { new Text("First Name: "), new Text("Middle Name:"), new Text("Last Name:"),
+                new Text("Age: "), new Text("Birth Month:"), new Text("Birth Day:"), new Text("Birth Year: "),
+                new Text("Address: "), new Text("Social Secrutiy Numbers:"), new Text("Race: "), new Text("Gender: "),
+                new Text("Married Status: "), new Text("Phone Numbers: "), new Text("Mother Name: "),
+                new Text("Father Name: "), new Text("Employment Status: "), new Text("Immigrant PID: "),
+                new Text("Immirgrant's Dependent PID: "), };
+        Text stringForm = new Text("Resigter for Dependent for Immigrant");
+        Text stringImmigrant = new Text("Immigrant Resigter");
+        stringForm.setFont(Font.font("seirf", FontWeight.BOLD, FontPosture.REGULAR, 50));
         GridPane approvalGridPane = new GridPane();
-        ScrollPane approvalScrollPane = new ScrollPane();
         approvalGridPane.setHgap(10);
-        approvalGridPane.setVgap(50);
+        approvalGridPane.setVgap(10);
+        approvalGridPane.setMaxSize(1920, 1080);
         Button b = new Button();
         b.setText("Hello Do you Want Approval");
-        // StackPane layout = new StackPane();
-        // layout.getChildren().add(b);
-        // approvalGridPane.add(b, x_grid, 1);
-        approvalScrollPane.setContent(b);
-        approvalGridPane.add(approvalScrollPane, x_grid / 2, y_grid / 2);
-        approvalScene = new Scene(approvalGridPane, 960, 540);
+        approvalGridPane.add(stringImmigrant, 1, 0);
+        for (int i = 0; i < approvalTextsI.length; i++) {
+            int column = i + 1;
+            approvalGridPane.add(approvalTextsI[i], 1, column);
+
+        }
+        for (int i = 0; i < approvalTextsD.length; i++) {
+            int column = i + 1;
+            approvalGridPane.add(approvalTextsD[i], 50, column);
+        }
+        approvalScene = new Scene(approvalGridPane, 1920, 1080);
         primaryStage.setScene(approvalScene);
         approvalScene.getRoot().setStyle("-fx-font-family: 'serif'");
 
